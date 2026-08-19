b6b151c24fe6229a0e71f3e42e7dc41f31e830ac
abcs2 <abcs2@cesar.school>
2026-08-19 16:41:36 -0300

    add lista de possiveis comandos

--- abcs2/source/main.c
+++ abcs2/source/main.c
@@ -8,6 +8,28 @@
 #define COMMAND_SIZE 20
 #define ARG_SIZE 20
 
+
+typedef struct task {
+	char name[NAME_SIZE];
+	char command[COMMAND_SIZE];
+	char** args;
+	struct task *next;
+} Task;
+
+typedef struct {
+	Task *head;
+} TaskList;
+
+
+void freeTaskList(Task *task) {
+    if (task == NULL) {
+        return;
+    }
+    freeTaskList(task->next);
+    free(task);
+    task = NULL;
+}
+
 int getQtd(char *str) {
     // Pegar qtd de argumentos na string de input
     int qtd = 0, i = 0;
@@ -30,11 +52,19 @@ char **getCommand(char *str, int qtd) {
     char aux = ' ', prev = ' ', auxString[NAME_SIZE] = "", aux2[2] = " ";
     char **array = (char **) malloc(qtd * sizeof(char *));
     if (array == NULL) {
+        printf("Falha ao processar comando.\n");
         return NULL;
     }
     for (int i=0; i<qtd; i++) {
         array[i] = (char *) malloc(NAME_SIZE * sizeof(char));
         if (array[i] == NULL) {
+            printf("Falha ao processar comando.\n");
+            for (int j=(i-1); j>=0; j--) {
+                free(array[j]);
+                array[j] = NULL;
+            }
+            free(array);
+            array = NULL;
             return NULL;
         }
     }
@@ -72,7 +102,79 @@ char **getCommand(char *str, int qtd) {
     return array;
 }
 
-void processString(char *str) {
+
+void newTask(TaskList *taskList, char **array, int qtd) {
+    if (qtd < 4) {
+        printf("Numero de comandos invalidos.\n");
+        return;
+    }
+
+    Task *task = (Task *) malloc(sizeof(Task)), *aux;
+    if (task == NULL) {
+        printf("Falha ao adicionar task.\n");
+        return;
+    }
+    strcpy(task->name, array[1]);
+    strcpy(task->command, array[2]);
+
+    task->args = (char **) malloc((qtd-3) * sizeof(char *));
+    if (task->args == NULL) {
+        printf("Falha ao adicionar task.\n");
+        free(task);
+        task = NULL;
+        return;
+    }
+    // printf("Antes do for arg\n");
+    for (int i=3; i<qtd; i++) {
+        (task->args)[i-3] = (char *) malloc(ARG_SIZE * sizeof(char));
+        if ((task->args)[i-3] == NULL) {
+            printf("Falha ao adicionar task.\n");
+            for (int j=(i-4); j>=0; j--) {
+                free((task->args)[j]);
+                (task->args)[j] = NULL;
+            }
+            free(task);
+            task = NULL;
+            return;
+        }
+        // printf("Antes do strcpy\n");
+        if (array[i] != NULL) {
+            strcpy((task->args)[i-3], array[i]);
+        } else {
+            (task->args)[i-3] = NULL;
+        }
+    }
+    // printf("Depois do for arg\n");
+    task->next = NULL;
+
+    if (taskList->head == NULL) {
+        taskList->head = task;
+    } else {
+        aux = taskList->head;
+        while (aux->next != NULL) {
+            aux = aux->next;
+        }
+        aux->next = task;
+    }
+
+    printf("Nome: %s\n", task->name);
+    printf("Comando: %s\n", task->command);
+    printf("Argumentos: ");
+    for (int i=3; i<qtd; i++) {
+        if ((task->args)[i-3] != NULL) {
+            printf("%s, ", (task->args)[i-3]);
+        } else {
+            printf("NULL");
+        }
+        
+    }
+    printf("\n");
+
+    return;
+}
+
+
+void processString(TaskList *taskList, char *str) {
     int qtd = getQtd(str) + 1;
     // printf("%s\nQtd: %d + 1\n", str, qtd-1);
     char **array = getCommand(str, qtd);
@@ -87,17 +189,33 @@ void processString(char *str) {
     }
     printf("]\n");
 
+    if (strcmp(array[0], "task") == 0) {
+        newTask(taskList, array, qtd);
+    }
+    else if (strcmp(array[0], "run") == 0) {
+
+    }
+
     free(array);
+    array = NULL;
     return;
 }
 
+
 int main(int argc, char** argv) {
-    char str[INPUT_SIZE];
     if (argc != 1 && argc != 2) {
         printf("Quantidade invalida de argumentos.\n");
         return 1;
     }
-    else if (argc == 1) {
+    TaskList *taskList = (TaskList *) malloc(sizeof(TaskList));
+    if (taskList == NULL) {
+        printf("Falha ao comecar o programa.\n");
+        return 1;
+    }
+    taskList->head = NULL;
+    char str[INPUT_SIZE];
+
+    if (argc == 1) {
         // Interativo
         while (1) {
             printf("processflow> ");
@@ -111,13 +229,17 @@ int main(int argc, char** argv) {
             //
 
             if (strcmp(str, "") != 0) {
-                processString(str);
+                processString(taskList, str);
             }
         }
     }
     else if (argc == 2) {
         // Workflow
     }
+
+    freeTaskList(taskList->head);
+    free(taskList);
+    taskList = NULL;
     
     return 0;
 }
